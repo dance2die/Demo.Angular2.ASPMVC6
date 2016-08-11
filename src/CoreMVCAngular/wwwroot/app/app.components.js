0137544be51c4628e0e69f6ad96c03e6b1f80f82
dance2die <dontbothermewithspam@gmail.com>
2016-08-11 11:51:36 -0400

    Added bower and npm packages

---
+++ src/CoreMVCAngular/wwwroot/app/app.components.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=app.components.js.map
---
+++ src/CoreMVCAngular/wwwroot/lib-npm/typings/es6-shim.js
@@ -0,0 +1,5 @@
+// Type definitions for es6-shim v0.31.2
+// Project: https://github.com/paulmillr/es6-shim
+// Definitions by: Ron Buckton <http://github.com/rbuckton>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+//# sourceMappingURL=es6-shim.js.map
