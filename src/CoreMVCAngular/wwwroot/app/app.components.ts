971fa215104ec5fe3f00a8dff2beda64fcb12b5a
dance2die <dontbothermewithspam@gmail.com>
2016-08-12 07:13:13 -0400

    Added views but angular 2 is not working yet.

--- src/CoreMVCAngular/wwwroot/app/app.components.ts
+++ src/CoreMVCAngular/wwwroot/app/app.components.ts
@@ -1,1 +1,7 @@
-﻿
+﻿import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'core-app',
+    template: '<h3>Please.Welcome to .NET Core 1.0 + MVC6 + Angular 2</h3>'
+})
+export class AppComponent { }
